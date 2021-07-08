41cc4ac0020d0cbe282a1ae18470981a1b9c5422
Roy Habeahan <hi@royryando.me>
2021-07-08 10:07:42 +0700

    change how it works title and image

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -124,7 +124,7 @@
     <section class="bg-white py-8">
         <div class="container max-w-5xl mx-auto m-8">
             <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-                How it Works?
+                How to Start?
             </h1>
             <div class="w-full mb-4">
                 <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
