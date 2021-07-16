4b87739567b68ed30a9ae73635a0e0f95fcf69be
Roy Habeahan <hi@royryando.me>
2021-07-16 21:03:12 +0700

    dynamic data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Campaign;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
 
     public function home() {
-        return view('app.home');
+        $tops = Campaign::where('status', 'ACTIVE')
+            ->orderBy('views', 'DESC')
+            ->limit(3)
+            ->get();
+        return view('app.home', compact('tops'));
     }
 
     public function browse() {
-        return view('app.browse');
+        $campaigns = Campaign::where('status', 'ACTIVE')
+            ->limit(30)
+            ->orderBy('views', 'DESC')
+            ->get();
+        return view('app.browse', compact('campaigns'));
     }
 
 }
